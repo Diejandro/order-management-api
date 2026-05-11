f2efb6eefd57c761a8fd422f897435126d1cf29c
Diego <diegalex93@gmail.com>
2026-05-11 18:44:31 +0200

    feat: implementar seguridad JWT, UserDetailsService y validación personalizada de usuario

--- src/main/java/com/diego/mi_primer_api/security/SpringSecurityConfig.java
+++ src/main/java/com/diego/mi_primer_api/security/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.diego.mi_primer_api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,7 +22,8 @@ public class SpringSecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http.authorizeHttpRequests((authz) -> authz
-                .requestMatchers("/api/users").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
                 .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
--- src/main/java/com/diego/mi_primer_api/services/UserServiceImpl.java
+++ src/main/java/com/diego/mi_primer_api/services/UserServiceImpl.java
@@ -6,7 +6,6 @@ import com.diego.mi_primer_api.repositories.RoleRepository;
 import com.diego.mi_primer_api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -58,6 +57,11 @@ public class UserServiceImpl implements UserService {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         return userRepository.save(user);
+    }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
     }
 
 }
